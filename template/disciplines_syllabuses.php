d4d7dce91d94ff6cbdd1ed33b9c4df1dfef19e06
Anastasia Pirogova <109249070+Nastia-Pirogova@users.noreply.github.com>
2024-01-31 15:26:14 +0200

    add3

---
+++ template/disciplines_syllabuses.php
@@ -0,0 +1,177 @@
+<?php
+/*
+ * Template Name: Disciplines and syllabuses
+*/
+get_header();
+?>
+<main>
+
+    <section class="home-hero disciplines_hero"
+             style="background-image: url(<?php echo the_field('disciplines_syllabuses_images'); ?>);">
+        <div class="container">
+            <h2 class="home-hero__title disciplines_hero__title">
+                <div><?php the_field("disciplines_syllabuses_title"); ?></div>
+                <div class="disciplines_hero_title-age"><?php the_field("disciplines_syllabuses_title_age"); ?></div>
+                <div class="disciplines_hero_title-spec"><?php the_field("disciplines_syllabuses_title_spec"); ?></div>
+            </h2>
+        </div>
+    </section>
+
+    <div class="disciplines_section">
+        <div class="container">
+            <div class="disciplines-inner">
+                <div class="disciplines-name">
+                    <div class="disciplines-name-inner">
+                        <div class="disciplines-name-inner-num">
+                            1
+                        </div>
+                        <div>
+                            <div class="disciplines-name-inner-name">Освітньо-професійна программа 2022р. рівня
+                                “Бакалавр”
+                            </div>
+                            <a href="" class="disciplines-name-inner-send">
+                                <span>Завантажити</span>
+                                <svg width="24px" height="24px" class="">
+                                    <use href="<?php echo bloginfo('template_url'); ?>/assets/images/icons/icons.svg#icon-send"></use>
+                                </svg>
+                            </a>
+                            <div class="disciplines-name-inner-descr">Перелік дисциплін та силабусів 1-8 семестрів</div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div class="disciplines-descr">
+                        <div class="disciplines-descr-text-1">ОБОВ’ЯЗКОВІ КОМПОНЕНТИ</div>
+                        <div class="disciplines-descr-text-2">Цикл дисциплін професійної підготовки</div>
+                    </div>
+                    <ul class="disciplines-list">
+                        <li class="disciplines-list-item">
+                            <div class="disciplines-list-item-title">1. Комп'ютерні технології та програмування</div>
+                            <div class="disciplines-list-item-author">(доцент Безкоровайний Ю.М.) семестр 1, екзамен.
+                            </div>
+                            <a href="" class="disciplines-list-item-send">
+                                <span>Silabus до дисципліни (pdf)</span>
+                                <svg width="24px" height="24px" class="">
+                                    <use href="<?php echo bloginfo('template_url'); ?>/assets/images/icons/icons.svg#icon-send"></use>
+                                </svg>
+                            </a>
+                        </li>
+                        <li class="disciplines-list-item">
+                            <div class="disciplines-list-item-title">1. Комп'ютерні технології та програмування</div>
+                            <div class="disciplines-list-item-author">(доцент Безкоровайний Ю.М.) семестр 1, екзамен.
+                            </div>
+                            <a href="" class="disciplines-list-item-send">
+                                <span>Silabus до дисципліни (pdf)</span>
+                                <svg width="24px" height="24px" class="">
+                                    <use href="<?php echo bloginfo('template_url'); ?>/assets/images/icons/icons.svg#icon-send"></use>
+                                </svg>
+                            </a>
+                        </li>
+                        <li class="disciplines-list-item">
+                            <div class="disciplines-list-item-title">1. Комп'ютерні технології та програмування</div>
+                            <div class="disciplines-list-item-author">(доцент Безкоровайний Ю.М.) семестр 1, екзамен.
+                            </div>
+                            <a href="" class="disciplines-list-item-send">
+                                <span>Silabus до дисципліни (pdf)</span>
+                                <svg width="24px" height="24px" class="">
+                                    <use href="<?php echo bloginfo('template_url'); ?>/assets/images/icons/icons.svg#icon-send"></use>
+                                </svg>
+                            </a>
+                        </li>
+                        <li class="disciplines-list-item">
+                            <div class="disciplines-list-item-title">1. Комп'ютерні технології та програмування</div>
+                            <div class="disciplines-list-item-author">(доцент Безкоровайний Ю.М.) семестр 1, екзамен.
+                            </div>
+                            <a href="" class="disciplines-list-item-send">
+                                <span>Silabus до дисципліни (pdf)</span>
+                                <svg width="24px" height="24px" class="">
+                                    <use href="<?php echo bloginfo('template_url'); ?>/assets/images/icons/icons.svg#icon-send"></use>
+                                </svg>
+                            </a>
+                        </li>
+                        <li class="disciplines-list-item">
+                            <div class="disciplines-list-item-title">1. Комп'ютерні технології та програмування</div>
+                            <div class="disciplines-list-item-author">(доцент Безкоровайний Ю.М.) семестр 1, екзамен.
+                            </div>
+                            <a href="" class="disciplines-list-item-send">
+                                <span>Silabus до дисципліни (pdf)</span>
+                                <svg width="24px" height="24px" class="">
+                                    <use href="<?php echo bloginfo('template_url'); ?>/assets/images/icons/icons.svg#icon-send"></use>
+                                </svg>
+                            </a>
+                        </li>
+
+
+                    </ul>
+                </div>
+                <div>
+                    <div class="disciplines-descr">
+                        <div class="disciplines-descr-text-1">ОБОВ’ЯЗКОВІ КОМПОНЕНТИ</div>
+                        <div class="disciplines-descr-text-2">Цикл дисциплін професійної підготовки</div>
+                    </div>
+                    <ul class="disciplines-list">
+                        <li class="disciplines-list-item">
+                            <div class="disciplines-list-item-title">1. Комп'ютерні технології та програмування</div>
+                            <div class="disciplines-list-item-author">(доцент Безкоровайний Ю.М.) семестр 1, екзамен.
+                            </div>
+                            <a href="" class="disciplines-list-item-send">
+                                <span>Silabus до дисципліни (pdf)</span>
+                                <svg width="24px" height="24px" class="">
+                                    <use href="<?php echo bloginfo('template_url'); ?>/assets/images/icons/icons.svg#icon-send"></use>
+                                </svg>
+                            </a>
+                        </li>
+                        <li class="disciplines-list-item">
+                            <div class="disciplines-list-item-title">1. Комп'ютерні технології та програмування</div>
+                            <div class="disciplines-list-item-author">(доцент Безкоровайний Ю.М.) семестр 1, екзамен.
+                            </div>
+                            <a href="" class="disciplines-list-item-send">
+                                <span>Silabus до дисципліни (pdf)</span>
+                                <svg width="24px" height="24px" class="">
+                                    <use href="<?php echo bloginfo('template_url'); ?>/assets/images/icons/icons.svg#icon-send"></use>
+                                </svg>
+                            </a>
+                        </li>
+                        <li class="disciplines-list-item">
+                            <div class="disciplines-list-item-title">1. Комп'ютерні технології та програмування</div>
+                            <div class="disciplines-list-item-author">(доцент Безкоровайний Ю.М.) семестр 1, екзамен.
+                            </div>
+                            <a href="" class="disciplines-list-item-send">
+                                <span>Silabus до дисципліни (pdf)</span>
+                                <svg width="24px" height="24px" class="">
+                                    <use href="<?php echo bloginfo('template_url'); ?>/assets/images/icons/icons.svg#icon-send"></use>
+                                </svg>
+                            </a>
+                        </li>
+                        <li class="disciplines-list-item">
+                            <div class="disciplines-list-item-title">1. Комп'ютерні технології та програмування</div>
+                            <div class="disciplines-list-item-author">(доцент Безкоровайний Ю.М.) семестр 1, екзамен.
+                            </div>
+                            <a href="" class="disciplines-list-item-send">
+                                <span>Silabus до дисципліни (pdf)</span>
+                                <svg width="24px" height="24px" class="">
+                                    <use href="<?php echo bloginfo('template_url'); ?>/assets/images/icons/icons.svg#icon-send"></use>
+                                </svg>
+                            </a>
+                        </li>
+                        <li class="disciplines-list-item">
+                            <div class="disciplines-list-item-title">1. Комп'ютерні технології та програмування</div>
+                            <div class="disciplines-list-item-author">(доцент Безкоровайний Ю.М.) семестр 1, екзамен.
+                            </div>
+                            <a href="" class="disciplines-list-item-send">
+                                <span>Silabus до дисципліни (pdf)</span>
+                                <svg width="24px" height="24px" class="">
+                                    <use href="<?php echo bloginfo('template_url'); ?>/assets/images/icons/icons.svg#icon-send"></use>
+                                </svg>
+                            </a>
+                        </li>
+
+
+                    </ul>
+                </div>
+            </div>
+        </div>
+</main>
+<?php
+get_footer();
+?>
