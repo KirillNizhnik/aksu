d2add87c6f0ee1748d1772fe93f02d8e548e13c0
Anastasia Pirogova <109249070+Nastia-Pirogova@users.noreply.github.com>
2024-02-04 21:00:27 +0200

    Add

---
+++ conferences-post.php
@@ -0,0 +1,133 @@
+<?php
+/*
+ * Template Name: Conferences-post
+*/
+get_header();
+?>
+<main>
+    <section class="conferences-post">
+        <div class="container">
+            <h1 class="conferences-post-title">Mатеріали другої міжнародної конференції "MATLAB та комп'ютерні
+                обчислення в освіті, науці та інженерії", Квітень 24 – 28, 2021 р., Київ
+            </h1>
+
+            <div class="conferences-post-wrap">
+                <div>24-27 квітня 2021 року на кафедрі аерокосмічних систем управління Факультету аеронавігації,
+                    електроніки
+                    та телекомунікацій Національного авіаційного університету відбулась ІІ Міжнародна наукова
+                    конференція
+                    «MATLAB та комп'ютерні обчислення в освіті, науці та інженерії». Співорганізаторами конференції
+                    виступили: Херсонський державний університет, IT-компанія GlobalLogic Ukraine, Education та компанія
+                    MathWorks Inc. (Netherland branch). У роботі конференції взяли участь науковці, викладачі та
+                    студенти з
+                    України та Китаю.
+
+                    Матеріали конференції публікуються лише з дозволу авторів
+                </div>
+                <img src="" alt="" class="conferences-post-img">
+            </div>
+            <div class="conferences-post-swiper swiper">
+                <div class="swiper-wrap">
+                    <div>
+                        <img src="" alt="" class="">
+                    </div>
+                    <div>
+                        <img src="" alt="" class="">
+                    </div>
+                    <div>
+                        <img src="" alt="" class="">
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="conferences-post-inner">
+                <div>
+                    <h3 class="conferences-post-inner-title">Публікація доповідей буде проводитися в порядку їх
+                        надходження
+                    </h3>
+                    <div class="conferences-post-inner-list">
+                        <div class="conferences-post-inner-list-item">
+                            <div>
+                                Конін В.В., Погурельський О.С., Малютенко Т.Л., Приходько І.А., Сушич О.П. (Київ,
+                                Національний авіаційний університет)Дистанційне дослідження глобальних навігаційних
+                                супутникових систем,Remote research of global navigation satellite systems
+                            </div>
+                            <div class="conferences-post-btns">
+                            <a href="" class="btn-section">
+                                <span>Вимоги до тез доповідей</span>
+                                <svg width="24px" height="24px"
+                                     class="home-history__btn-svg ">
+                                    <use href="<?php echo bloginfo('template_url'); ?>/assets/images/icons/icons.svg#icon-arrow-mini-top"></use>
+                                </svg>
+                            </a>
+                            <a href="" class="btn-section">
+                                <span>Вимоги до тез доповідей</span>
+                                <svg width="24px" height="24px"
+                                     class="home-history__btn-svg ">
+                                    <use href="<?php echo bloginfo('template_url'); ?>/assets/images/icons/icons.svg#icon-send"></use>
+                                </svg>
+                            </a>
+                        </div>
+
+
+                </div>
+                        <div class="conferences-post-inner-list-item">
+                            <div>
+                                Конін В.В., Погурельський О.С., Малютенко Т.Л., Приходько І.А., Сушич О.П. (Київ,
+                                Національний авіаційний університет)Дистанційне дослідження глобальних навігаційних
+                                супутникових систем,Remote research of global navigation satellite systems
+                            </div>
+                            <div class="conferences-post-btns">
+                                <a href="" class="btn-section">
+                                    <span>Вимоги до тез доповідей</span>
+                                    <svg width="24px" height="24px"
+                                         class="home-history__btn-svg ">
+                                        <use href="<?php echo bloginfo('template_url'); ?>/assets/images/icons/icons.svg#icon-arrow-mini-top"></use>
+                                    </svg>
+                                </a>
+                                <a href="" class="btn-section">
+                                    <span>Вимоги до тез доповідей</span>
+                                    <svg width="24px" height="24px"
+                                         class="home-history__btn-svg ">
+                                        <use href="<?php echo bloginfo('template_url'); ?>/assets/images/icons/icons.svg#icon-send"></use>
+                                    </svg>
+                                </a>
+                            </div>
+
+
+                        </div>
+                        <div class="conferences-post-inner-list-item">
+                            <div>
+                                Конін В.В., Погурельський О.С., Малютенко Т.Л., Приходько І.А., Сушич О.П. (Київ,
+                                Національний авіаційний університет)Дистанційне дослідження глобальних навігаційних
+                                супутникових систем,Remote research of global navigation satellite systems
+                            </div>
+                            <div class="conferences-post-btns">
+                                <a href="" class="btn-section">
+                                    <span>Вимоги до тез доповідей</span>
+                                    <svg width="24px" height="24px"
+                                         class="home-history__btn-svg ">
+                                        <use href="<?php echo bloginfo('template_url'); ?>/assets/images/icons/icons.svg#icon-arrow-mini-top"></use>
+                                    </svg>
+                                </a>
+                                <a href="" class="btn-section">
+                                    <span>Вимоги до тез доповідей</span>
+                                    <svg width="24px" height="24px"
+                                         class="home-history__btn-svg ">
+                                        <use href="<?php echo bloginfo('template_url'); ?>/assets/images/icons/icons.svg#icon-send"></use>
+                                    </svg>
+                                </a>
+                            </div>
+
+
+                        </div>
+            </div>
+        </div>
+    </section>
+
+</main>
+<?php
+get_footer();
+?>
+
---
+++ conferences.php
@@ -0,0 +1,158 @@
+<?php
+/*
+ * Template Name: Conferences
+*/
+get_header();
+?>
+<main>
+    <section class="home-hero"
+             style="background-image: url(<?php echo the_field('conferences_img'); ?>); background-color: #084184">
+        <div class="container">
+            <h2 class="home-hero__title"><?php the_field("conferences_title"); ?></h2>
+        </div>
+    </section>
+
+    <section class="conferences">
+        <div class="container">
+            <ul class="conferences-list">
+                <li class="conferences-list__item">
+                    <img src="" alt="" class="conferences-list__item-img">
+                    <div class="conferences-list__item-wrapp">
+                        <div>
+                            <h3 class="conferences-list__item-title">Науково-технічна конференція «Сучасні технології
+                                розробки комп’ютеризованих систем керування рухом» Київ / НАУ / листопад 2023</h3>
+                            <p class="conferences-list__item-descr">Кафедра аерокосмічних систем управління, Факультету
+                                аеронавігації, електроніки та телекомунікацій запрошують студентів та молодих учених
+                                взяти
+                                участь у щорічній науково-технічній конференції«Сучасні технології розвитку
+                                комп’ютеризованих систем керування рухом».Участь у Конференції безкоштовна. За
+                                результатами
+                                Конференції буде видано електронний збірник тез.</p>
+                        </div>
+
+                        <div class="conferences-list__item-btns">
+                            <a href="" class="btn-section">
+                                <span>Вимоги до тез доповідей</span>
+                                <svg width="24px" height="24px"
+                                     class="home-history__btn-svg ">
+                                    <use href="<?php echo bloginfo('template_url'); ?>/assets/images/icons/icons.svg#icon-arrow-mini-top"></use>
+                                </svg>
+                            </a>
+                            <a href="" class="btn-section">
+                                <span>Вимоги до тез доповідей</span>
+                                <svg width="24px" height="24px"
+                                     class="home-history__btn-svg ">
+                                    <use href="<?php echo bloginfo('template_url'); ?>/assets/images/icons/icons.svg#icon-send"></use>
+                                </svg>
+                            </a>
+                        </div>
+                    </div>
+                </li>
+                <li class="conferences-list__item">
+                    <img src="" alt="" class="conferences-list__item-img">
+                    <div class="conferences-list__item-wrapp">
+                        <div>
+                            <h3 class="conferences-list__item-title">Науково-технічна конференція «Сучасні технології
+                                розробки комп’ютеризованих систем керування рухом» Київ / НАУ / листопад 2023</h3>
+                            <p class="conferences-list__item-descr">Кафедра аерокосмічних систем управління, Факультету
+                                аеронавігації, електроніки та телекомунікацій запрошують студентів та молодих учених
+                                взяти
+                                участь у щорічній науково-технічній конференції«Сучасні технології розвитку
+                                комп’ютеризованих систем керування рухом».Участь у Конференції безкоштовна. За
+                                результатами
+                                Конференції буде видано електронний збірник тез.</p>
+                        </div>
+
+                        <div class="conferences-list__item-btns">
+                            <a href="" class="btn-section">
+                                <span>Вимоги до тез доповідей</span>
+                                <svg width="24px" height="24px"
+                                     class="home-history__btn-svg">
+                                    <use href="<?php echo bloginfo('template_url'); ?>/assets/images/icons/icons.svg#icon-send"></use>
+                                </svg>
+                            </a>
+                            <a href="" class="btn-section">
+                                <span>Вимоги до тез доповідей</span>
+                                <svg width="24px" height="24px"
+                                     class="home-history__btn-svg">
+                                    <use href="<?php echo bloginfo('template_url'); ?>/assets/images/icons/icons.svg#icon-send"></use>
+                                </svg>
+                            </a>
+                        </div>
+                    </div>
+                </li>
+                <li class="conferences-list__item">
+                    <img src="" alt="" class="conferences-list__item-img">
+                    <div class="conferences-list__item-wrapp">
+                        <div>
+                            <h3 class="conferences-list__item-title">Науково-технічна конференція «Сучасні технології
+                                розробки комп’ютеризованих систем керування рухом» Київ / НАУ / листопад 2023</h3>
+                            <p class="conferences-list__item-descr">Кафедра аерокосмічних систем управління, Факультету
+                                аеронавігації, електроніки та телекомунікацій запрошують студентів та молодих учених
+                                взяти
+                                участь у щорічній науково-технічній конференції«Сучасні технології розвитку
+                                комп’ютеризованих систем керування рухом».Участь у Конференції безкоштовна. За
+                                результатами
+                                Конференції буде видано електронний збірник тез.</p>
+                        </div>
+
+                        <div class="conferences-list__item-btns">
+                            <a href="" class="btn-section">
+                                <span>Вимоги до тез доповідей</span>
+                                <svg width="24px" height="24px"
+                                     class="home-history__btn-svg">
+                                    <use href="<?php echo bloginfo('template_url'); ?>/assets/images/icons/icons.svg#icon-send"></use>
+                                </svg>
+                            </a>
+                            <a href="" class="btn-section">
+                                <span>Вимоги до тез доповідей</span>
+                                <svg width="24px" height="24px"
+                                     class="home-history__btn-svg">
+                                    <use href="<?php echo bloginfo('template_url'); ?>/assets/images/icons/icons.svg#icon-send"></use>
+                                </svg>
+                            </a>
+                        </div>
+                    </div>
+                </li>
+                <li class="conferences-list__item">
+                    <img src="" alt="" class="conferences-list__item-img">
+                    <div class="conferences-list__item-wrapp">
+                        <div>
+                            <h3 class="conferences-list__item-title">Науково-технічна конференція «Сучасні технології
+                                розробки комп’ютеризованих систем керування рухом» Київ / НАУ / листопад 2023</h3>
+                            <p class="conferences-list__item-descr">Кафедра аерокосмічних систем управління, Факультету
+                                аеронавігації, електроніки та телекомунікацій запрошують студентів та молодих учених
+                                взяти
+                                участь у щорічній науково-технічній конференції«Сучасні технології розвитку
+                                комп’ютеризованих систем керування рухом».Участь у Конференції безкоштовна. За
+                                результатами
+                                Конференції буде видано електронний збірник тез.</p>
+                        </div>
+
+                        <div class="conferences-list__item-btns">
+                            <a href="" class="btn-section">
+                                <span>Вимоги до тез доповідей</span>
+                                <svg width="24px" height="24px"
+                                     class="home-history__btn-svg">
+                                    <use href="<?php echo bloginfo('template_url'); ?>/assets/images/icons/icons.svg#icon-send"></use>
+                                </svg>
+                            </a>
+                            <a href="" class="btn-section">
+                                <span>Вимоги до тез доповідей</span>
+                                <svg width="24px" height="24px"
+                                     class="home-history__btn-svg">
+                                    <use href="<?php echo bloginfo('template_url'); ?>/assets/images/icons/icons.svg#icon-send"></use>
+                                </svg>
+                            </a>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+            <div class="conferences-pagination"></div>
+        </div>
+    </section>
+
+</main>
+<?php
+get_footer();
+?>
