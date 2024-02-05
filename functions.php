ecc2384b692b75eed6997fbcb6aa0d1ffd4a57f7
Kirill Nizhnik <117679943+KirillNizhnik@users.noreply.github.com>
2024-02-05 14:58:08 +0200

    news

--- functions.php
+++ functions.php
@@ -148,7 +148,7 @@ if( function_exists('acf_add_options_page') ) {
 /**
  * Enqueue scripts and styles.
  */
-function aksu_scripts() {
+function aksu_scripts(): void {
 	wp_enqueue_style( 'aksu-style', get_stylesheet_uri(), array(), _S_VERSION );
 	wp_style_add_data( 'aksu-style', 'rtl', 'replace' );
 
@@ -163,19 +163,23 @@ function aksu_scripts() {
     wp_style_add_data('aksu-style', 'rtl', 'replace');
     wp_deregister_script('jquery');
     wp_enqueue_script('jquery', 'https://code.jquery.com/jquery-3.6.4.min.js', array(), null, true);
-//    wp_enqueue_script('aksu-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true);
     if (is_singular() && comments_open() && get_option('thread_comments')) {
         wp_enqueue_script('comment-reply');
     }
-//    wp_enqueue_script('aksu-mobile-menu', get_template_directory_uri() . '/assets/js/mobile-menu.js', array(), null, true);
-//    wp_enqueue_script('aksu-modal-partners', get_template_directory_uri() . '/assets/js/modal-partners.js', array('jquery'), null, true);
+
     wp_enqueue_script('swiper', 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js', array(), null, true);
-    wp_enqueue_script('swiper-1', 'https://unpkg.com/swiper@6.5.4/swiper-bundle.min.js', array(), null, true);
-    wp_enqueue_script('aksu-swiper-home-news', get_template_directory_uri() . '/assets/scripts/main-news-slider.js', array('jquery'), null, true);
+//    wp_enqueue_script('swiper-1', 'https://unpkg.com/swiper@6.5.4/swiper-bundle.min.js', array(), null, true);
+	wp_enqueue_script('aksu-swiper-home-news', get_template_directory_uri() . '/assets/scripts/main-news-slider.js', array('jquery'), null, true);
+	if (is_singular('news')){
+		wp_enqueue_script('second-news-slider', get_template_directory_uri() . '/assets/scripts/news-post-slider.js' , array('jquery'), null, true);
+	}
 
 
 }
+
 add_action( 'wp_enqueue_scripts', 'aksu_scripts' );
+
+
 add_theme_support('custom-logo');
 add_theme_support('post-thumbnails');
 add_theme_support('menus');
@@ -209,22 +213,7 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 // news-post-type.php
 require_once(get_template_directory() . '/news-post-type.php');
 
-$args = array(
-    'post_type' => 'news',
-    'posts_per_page' => -1,
-);
 
-$query = new WP_Query($args);
-
-if ($query->have_posts()) :
-    while ($query->have_posts()) : $query->the_post();
-        the_title();
-        the_content();
-    endwhile;
-    wp_reset_postdata();
-else :
-    echo 'Посты не найдены.';
-endif;
 
 
 
---
+++ single-news.php
@@ -0,0 +1,105 @@
+<?php
+get_header();
+?>
+
+    <main>
+        <section class="news-page">
+            <div class="news-page__photo">
+                <img src="<?php echo get_the_post_thumbnail_url() ?>" alt="" class="news-page__img">
+                <div class="news-page__title title-section"><?php the_title() ?></div>
+            </div>
+            <div class="container news-page__container">
+                <div class="news-page__main-content">
+                    <div class="news-page__date"><?php echo get_the_date( 'Y.d.m' ); ?></div>
+                    <p class="news-page__text">
+						<?php echo get_the_content(); ?>
+                    </p>
+                    <div class="swiper news-page__swiper">
+                        <div class="news-page__swiper-wrapper swiper-wrapper">
+	                        <?php if( have_rows('news_gellery_images') ): ?>
+
+			                        <?php while( have_rows('news_gellery_images') ): the_row();
+				                        $image = get_sub_field('news_image');
+				                        ?>
+                                        <img src="<?php echo $image['url']; ?>"
+                                             alt="<?php echo $image['alt']; ?>"
+                                             class="news-page__swiper-slide swiper-slide">
+			                        <?php endwhile; ?>
+
+	                        <?php endif; ?>
+
+                        </div>
+                        <div class="news-page__swiper-pagination swiper-pagination"></div>
+                        <div class="news-page__buttons">
+                            <div class="news-page__button-prev">
+                                <svg width="80px" height="80px">
+                                    <use href="<?php echo bloginfo( 'template_url' ); ?>/assets/images/icons/icons.svg#icon-arrow-left"></use>
+                                </svg>
+                            </div>
+                            <div class="news-page__button-next">
+                                <svg width="80px" height="80px">
+                                    <use href="<?php echo bloginfo( 'template_url' ); ?>/assets/images/icons/icons.svg#icon-arrow-right"></use>
+                                </svg>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="news-sidebar">
+                    <div class="news-sidebar__top">
+                        <div class="news-sidebar__title">Інші новини</div>
+                        <a href="<?php echo get_post_type_archive_link('news');
+                        ?>" class="news-sidebar__link btn-section">Всі новини
+                            <svg width="32px" height="32px">
+                                <use href="<?php echo bloginfo( 'template_url' ); ?>/assets/images/icons/icons.svg#icon-arrow-mini-top"></use>
+                            </svg>
+                        </a>
+                    </div>
+                    <div class="swiper news-sidebar__swiper">
+                        <div class="news-sidebar__swiper-wrapper swiper-wrapper">
+							<?php
+
+							$args_sidebar = array(
+								'post_type'      => 'news',
+								'orderby'        => 'date',
+								'order'          => 'DESC',
+								'posts_per_page' => 3,
+							);
+
+							$query_sidebar = new WP_Query( $args_sidebar );
+
+							if ( $query_sidebar->have_posts() ) :
+								while ( $query_sidebar->have_posts() ) :
+									$query_sidebar->the_post();
+                            ?>
+                                    <div class="news-sidebar__swiper-slide swiper-slide">
+                                        <img src="<?php echo get_the_post_thumbnail_url() ?>" alt=""
+                                             class="news-sidebar__swiper-photo">
+                                        <div class="news-sidebar__swiper-content">
+                                            <div class="news-sidebar__swiper-title title-section"><?php the_title(); ?>
+                                            </div>
+                                            <div class="news-sidebar__swiper-inner">
+                                                <div class="news-sidebar__swiper-data"><?php echo get_the_date('Y.m.d')?></div>
+                                                <div class="news-sidebar__swiper-text"><?php echo get_the_excerpt(); ?>
+                                                </div>
+                                                <a href="<?php the_permalink(); ?>" class="news-sidebar__swiper-link btn-section">Читати більше
+                                                    <svg width="32px" height="32px">
+                                                        <use href="<?php echo bloginfo( 'template_url' ); ?>/assets/images/icons/icons.svg#icon-arrow-mini-top"></use>
+                                                    </svg>
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                <?php
+								endwhile;
+							endif;
+							?>
+                        <div class="news-sidebar__swiper-pagination swiper-pagination"></div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+
+
+<?php
+get_footer();
