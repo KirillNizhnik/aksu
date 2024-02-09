02dcb67921e7d635766805fd455c509ba4e534e4
yarikleva <levchenko153729@gmail.com>
2024-02-09 20:14:05 +0200

    educational-materials php

---
+++ template/educational-materials.php
@@ -0,0 +1,52 @@
+<?php
+/*
+ * Template Name: educational-materials
+*/
+get_header();
+?>
+
+
+<main>
+    <section class="educational-materials">
+        <img src="images/educational-materials__background.jpg" alt="" class="educational-materials__photo">
+        <h2 class="educational-materials__title title-section">Навчальні матеріали</h2>
+        <div class="container educational-materials__container">
+            <div class="educational-materials__content">
+                <h2 class="educational-materials__content-title">Ресурси для моїх студентів</h2>
+                <p class="educational-materials__content-text">Сайт розроблений С.І. Вороновим за участю Т.А. Галагуз та
+                    Є.О. Гаєва . Основним призначенням сайту є надання наступних учбових матеріалів: <br>
+                    Сайт розроблений С.І. Вороновим за участю Т.А. Галагуз та
+                    Є.О. Гаєва . Основним призначенням сайту є надання наступних учбових матеріалів:
+                    <br> <br> <br>
+                    Сайт розроблений С.І. Вороновим за участю Т.А. Галагуз та
+                    Є.О. Гаєва . Основним призначенням сайту є надання наступних учбових матеріалів: <br>
+                    Сайт розроблений С.І. Вороновим за участю Т.А. Галагуз та
+                    Є.О. Гаєва . Основним призначенням сайту є надання наступних учбових матеріалів:
+                </p>
+                <a href="#" class="educational-materials__content-link btn-section">Перейти на сайт
+                    <svg width="32px" height="32px">
+                        <use href="images/icons/icons.svg#icon-arrow-mini-top"></use>
+                    </svg>
+                </a>
+            </div>
+            <div class="educational-materials__content">
+                <div class="educational-materials__content-title">Ресурси для моїх студентів</div>
+                <p class="educational-materials__content-text">Сайт розроблений С.І. Вороновим за участю Т.А. Галагуз та
+                    Є.О. Гаєва . Основним призначенням сайту є надання наступних учбових матеріалів: <br>
+                    Сайт розроблений С.І. Вороновим за участю Т.А. Галагуз та
+                    Є.О. Гаєва . Основним призначенням сайту є надання наступних учбових матеріалів:
+                </p>
+                <a href="#" class="educational-materials__content-link btn-section">Перейти на сайт
+                    <svg width="32px" height="32px">
+                        <use href="images/icons/icons.svg#icon-arrow-mini-top"></use>
+                    </svg>
+                </a>
+            </div>
+
+
+        </div>
+    </section>
+</main>
+
+
+<?php get_footer();?>
