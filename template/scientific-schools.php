afb823f03f1191090f034aa3793c0e7303a4722f
yarikleva <levchenko153729@gmail.com>
2024-02-11 20:14:16 +0200

    scientific-schools add

--- template/scientific-schools.php
+++ template/scientific-schools.php
@@ -6,26 +6,26 @@ get_header();
 ?>
     <main>
         <section class="scientific-schools">
-            <div class="container scientific-schools__container">
-                <h2 class="scientific-schools__title title-section"><?php the_field("scientific-schools_title"); ?></h2>
-                <?php if (have_rows('scientific-schools_repeater')): ?>
+            <div class="container">
+                <h2 class="scientific-schools__title title-section"><?php the_field("scientific_schools_title"); ?></h2>
+                <?php if (have_rows('scientific_schools_repeater')): ?>
                     <ul class="scientific-schools__content-wrap">
-                        <?php while (have_rows('scientific-schools_repeater')): the_row();
-                            $scientific_schools_title = get_sub_field('scientific-schools_title2'); ?>
+                        <?php while (have_rows('scientific_schools_repeater')): the_row();
+                            $scientific_schools_title = get_sub_field('scientific_schools_publications_title'); ?>
                             <li class="scientific-schools__content-inner">
                                 <h3 class="scientific-schools__title2"><?php echo $scientific_schools_title ?></h3>
-                                <?php if (have_rows('scientific-schools_item-repeater')): ?>
+                                <?php if (have_rows('scientific_schools_item_repeater')): ?>
                                     <ul class="scientific-schools__content-inner">
-                                        <?php while (have_rows('scientific-schools_item-repeater')): the_row();
-                                            $scientific_schools_content_title = get_sub_field('scientific-schools_content-title');
-                                            $scientific_schools_content_text1 = get_sub_field('scientific-schools_content-text1');
-                                            $scientific_schools_content_title2 = get_sub_field('scientific-schools_content-title2');
-                                            $scientific_schools_content_text2 = get_sub_field('scientific-schools_content-text2'); ?>
+                                        <?php while (have_rows('scientific_schools_item_repeater')): the_row();
+                                            $scientific_schools_content_title = get_sub_field('scientific_schools_content_title');
+                                            $scientific_schools_content_text = get_sub_field('scientific_schools_content_text');
+                                            $scientific_schools_head_of_direction_title = get_sub_field('scientific_schools_head_of_direction_title');
+                                            $scientific_schools_head_of_direction_full_name = get_sub_field('scientific_schools_head_of_direction_full_name'); ?>
                                             <li class="scientific-schools__content">
                                                 <div class="scientific-schools__content-title"><?php echo $scientific_schools_content_title ?></div>
-                                                <p class="scientific-schools__content-text1"><?php echo $scientific_schools_content_text1 ?></p>
-                                                <div class="scientific-schools__content-title"><?php echo $scientific_schools_content_title2 ?></div>
-                                                <p class="scientific-schools__content-text2"><?php echo $scientific_schools_content_text2 ?></p>
+                                                <p class="scientific-schools__content-text"><?php echo $scientific_schools_content_text ?></p>
+                                                <div class="scientific-schools__content-title"><?php echo $scientific_schools_head_of_direction_title ?></div>
+                                                <p class="scientific-schools__content-full-name"><?php echo $scientific_schools_head_of_direction_full_name ?></p>
                                             </li>
                                         <?php endwhile; ?>
                                     </ul>
