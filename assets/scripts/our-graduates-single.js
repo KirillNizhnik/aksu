ac56bcfd7223bfd5f512c621e7f32ffd937fa991
yarikleva <levchenko153729@gmail.com>
2024-02-25 00:03:24 +0200

    front

--- assets/scripts/our-graduates-single.js
+++ assets/scripts/our-graduates-single.js
@@ -1,4 +1,3 @@
-// Находим элементы, которые будем использовать
 const openPopupButtons = document.querySelectorAll('[data-popup-open]');
 const closePopupButtons = document.querySelectorAll('[data-popup-close]');
 const popup = document.querySelector('[data-popup]');
@@ -6,13 +5,13 @@ const popup = document.querySelector('[data-popup]');
 function openPopup() {
     popup.classList.add('is-active');
     document.body.classList.add('popup-open');
-    document.body.style.overflow = 'hidden'; // Видаляємо прокрутку тіла
+    document.body.style.overflow = 'hidden';
 }
 
 function closePopup() {
     popup.classList.remove('is-active');
     document.body.classList.remove('popup-open');
-    document.body.style.overflow = ''; // Повертаємо прокрутку тіла
+    document.body.style.overflow = '';
 }
 
 openPopupButtons.forEach(button => {
