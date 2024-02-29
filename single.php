3997aae6f266bb8263f400e7c4f9fc0aa0b5f4f4
yarikleva <levchenko153729@gmail.com>
2024-02-29 21:54:41 +0200

    fix-graduates-single-page

--- single.php
+++ single.php
@@ -9,32 +9,27 @@
 
 get_header();
 ?>
-
 	<main id="primary" class="site-main">
-
+        <div class="container">
 		<?php
 		while ( have_posts() ) :
 			the_post();
-
 			get_template_part( 'template-parts/content', get_post_type() );
-
 			the_post_navigation(
 				array(
 					'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'aksu' ) . '</span> <span class="nav-title">%title</span>',
 					'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'aksu' ) . '</span> <span class="nav-title">%title</span>',
 				)
 			);
-
-			// If comments are open or we have at least one comment, load up the comment template.
+            // If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
 				comments_template();
 			endif;
 
 		endwhile; // End of the loop.
 		?>
-
+        </div>
 	</main><!-- #main -->
-
 <?php
 get_sidebar();
 get_footer();
