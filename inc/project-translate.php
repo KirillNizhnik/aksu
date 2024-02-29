10c998c74cf0e5fd39983e91e34ed30e255b0acc
Kirill Nizhnik <117679943+KirillNizhnik@users.noreply.github.com>
2024-02-29 14:06:54 +0200

    hot-fix

--- inc/project-translate.php
+++ inc/project-translate.php
@@ -1,28 +1,30 @@
 <?php
-function my_custom_strings() {
-    $strings = array(
-        'page_error' => 'Page error',
-        'page_error_text' => 'Page error text',
-        'page_error_btn' => 'Page error button',
-        'phone_number_head_department' => 'Номер телефону завідувача кафедри',
-        'address' => 'Адреса',
-        'address_link' => 'Адреса(link)',
-        'maps_link' => 'Карта(link)',
-        'copyright' => 'Copyright',
-        'news_title' => 'News title',
-        'all_news_title' => 'All news title',
-        'send_btn' => 'Завантажити',
-        'learn_more_btn' => 'Дізнатись більше',
-        'other_news_title' => 'Other news title',
-        'read_more' => 'Читати більше',
-        'conferences_title' => 'Conferences title',
-        'teacher_title' => 'Teacher title',
-        'return_btn' => 'Return button',
-    );
+if (!function_exists('my_strings')) {
+	function my_strings() {
+		$strings = array(
+			'page_error'                   => 'Page error',
+			'page_error_text'              => 'Page error text',
+			'page_error_btn'               => 'Page error button',
+			'phone_number_head_department' => 'Номер телефону завідувача кафедри',
+			'address'                      => 'Адреса',
+			'address_link'                 => 'Адреса(link)',
+			'maps_link'                    => 'Карта(link)',
+			'copyright'                    => 'Copyright',
+			'news_title'                   => 'News title',
+			'all_news_title'               => 'All news title',
+			'send_btn'                     => 'Завантажити',
+			'learn_more_btn'               => 'Дізнатись більше',
+			'other_news_title'             => 'Other news title',
+			'read_more'                    => 'Читати більше',
+			'conferences_title'            => 'Conferences title',
+			'teacher_title'                => 'Teacher title',
+			'return_btn'                   => 'Return button',
+		);
 
-    foreach ($strings as $name => $original_text) {
-        pll_register_string($name, $original_text, 'aksu');
-    }
+		foreach ( $strings as $name => $original_text ) {
+			pll_register_string( $name, $original_text, 'aksu' );
+		}
+	}
+
+	add_action( 'after_setup_theme', 'my_strings' );
 }
-
-add_action('after_setup_theme', 'my_custom_strings');
